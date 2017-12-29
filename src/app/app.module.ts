d354c07c5c10dc57f4d927d5a065194c94e10cf8
jon.graca@gmail.com <jon.graca@gmail.com>
2017-12-29 11:03:11 -0500

    Adding additional component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import {JeopardyService } from './jeopardy.service';
 import { AnswerComponentComponent } from './answer-component/answer-component.component';
+import { CategoryComponentComponent } from './category-component/category-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AnswerComponentComponent
+    AnswerComponentComponent,
+    CategoryComponentComponent
   ],
   imports: [
     BrowserModule,
