2b1faae7a9e468ffbe1dabf128968895bc7fa359
jon.graca@gmail.com <jon.graca@gmail.com>
2017-12-29 15:30:25 -0500

    Removing un-used component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import {JeopardyService } from './jeopardy.service';
 import { AnswerComponentComponent } from './answer-component/answer-component.component';
-import { CategoryComponentComponent } from './category-component/category-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AnswerComponentComponent,
-    CategoryComponentComponent
   ],
   imports: [
     BrowserModule,
